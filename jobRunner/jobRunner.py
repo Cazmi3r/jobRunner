94b9a1b95a7578dbde025b086cc6f17e8f0983e5
Cazmi3r <PaulikasA@gmail.com>
2024-09-26 10:11:05 -0400

    read job num

--- jobRunner/jobRunner.py
+++ jobRunner/jobRunner.py
@@ -3,3 +3,11 @@ def fib(n: int) -> int:
         return n
     else:
         return fib(n - 1) + fib(n - 2)
+
+
+class JobRunner:
+    def __init__(self, args) -> None:
+        self.job_num = args["job_num"]
+
+    def get_job_num(self) -> str:
+        return self.job_num
--- test/test_jobRunner.py
+++ test/test_jobRunner.py
@@ -1,4 +1,12 @@
-from jobRunner.jobRunner import fib
+import pytest
+
+from jobRunner.jobRunner import JobRunner, fib
+
+
+@pytest.fixture
+def job():
+    args = {"job_num": "9999"}
+    return JobRunner(args)
 
 
 def test_fib() -> None:
@@ -9,3 +17,7 @@ def test_fib() -> None:
     assert fib(4) == 3
     assert fib(5) == 5
     assert fib(10) == 55
+
+
+def test_job_num(job) -> None:
+    assert job.get_job_num() == "9999"
